7982bf199ff287023f59e0901e6a813996407a6c
jobex11 <joblawal33@gmail.com>
2023-07-12 04:06:28 +0100

    updated ai news

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "1580e20aa848fedb98a83f9a5196b1562e956eca572e1d8b807a3e2338fdd0dc/stage";
-
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: () => {},
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
     });
   }, []);
   return <div>Alan AI News Application</div>;
+  <NewsCards articles={newsArticles} />;
 };
 
 export default App;
